0ce03b4bb37cfa363987a681b751a2cdeae0445c
Dong H. Kim <ehdgus1524@gmail.com>
2022-12-08 00:08:12 +0900

    [CHANGED] make User

--- src/server/server.go
+++ src/server/server.go
@@ -17,19 +17,14 @@ import (
 const port = ":8080"
 
 type user struct {
-	name        string
-	age         string
-	gender      string
-	email       string
-	dateOfBirth string
+	name        string `json:"name"`
+	age         string `json:"age"`
+	gender      string `json:"gender"`
+	email       string `json:"email"`
+	dateOfBirth string `json:"dateOfBirth"`
 }
 
 var Users []user
-
-// func init() {
-
-//		http.ListenAndServe(port)
-//	}
 
 /*
 "/" 로 들어온 요청 핸들링
@@ -52,6 +47,7 @@ func hellowReturn(write http.ResponseWriter, read *http.Request) {
 	json.NewEncoder(write).Encode("Hello")
 }
 
+// 사용자 관련 요청
 func userSetReturn(write http.ResponseWriter, read *http.Request) {
 	fmt.Println("Endpoint: Users")
 
@@ -59,7 +55,7 @@ func userSetReturn(write http.ResponseWriter, read *http.Request) {
 
 	var parameterArray []string
 
-	json.NewEncoder(write).Encode(parameters)
+	// json.NewEncoder(write).Encode(parameters)
 
 	for params, value := range parameters {
 		fmt.Println(params, "=>", value)
@@ -67,10 +63,13 @@ func userSetReturn(write http.ResponseWriter, read *http.Request) {
 		parameterArray = append(parameterArray, value...)
 	}
 
-	json.NewEncoder(write).Encode(parameterArray)
+	var User []user
+
+	json.NewEncoder(write).Encode(User)
 	fmt.Fprintf(write, "User Request")
 }
 
+// 라우터 핸들러
 func handleRequests(users ...user) {
 	router := mux.NewRouter()
 
@@ -80,6 +79,7 @@ func handleRequests(users ...user) {
 
 	println("Listening: 8080")
 
+	// 에러 핸들링
 	err := http.ListenAndServe(port, router)
 
 	if err != nil {
