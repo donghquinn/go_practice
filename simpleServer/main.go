6145657df78854200463ba4310ed955d84f63f71
Kim Donghyun <kimdonghyun@Kimui-MacBookAir.local>
2023-01-01 21:58:24 +0900

    [CHANGED] registered route

--- simpleServer/main.go
+++ simpleServer/main.go
@@ -22,6 +22,9 @@ func main() {
 	// 라우트 / 에 대한 요청이 들어왔을 때, handler 함수 호출
 	http.HandleFunc("/", handler)
 
+	// 파일 업로드 요청 라우트에 대해 핸들링
+	http.HandleFunc("/upload", fileUpHandler)
+
 	// 서버는 8080 포트 - ListenAndServe는 가장 마지막 줄에
 	http.ListenAndServe(":8080", nil)
 }
