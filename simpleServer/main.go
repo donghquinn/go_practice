c1eec78cec6b67bf2807da14b59371ff44057207
Kim Donghyun <kimdonghyun@Kimui-MacBookAir.local>
2023-01-01 21:49:15 +0900

    [ADD] File upload Route

--- simpleServer/main.go
+++ simpleServer/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 )
+
+/*
+참고: https://github.com/SimonWaldherr/golang-examples/blob/master/advanced/httpupload.go
+*/
 
 func main() {
 	fmt.Println("Server is Starting")
@@ -20,4 +25,27 @@ func handler(response http.ResponseWriter, request *http.Request) {
 	fmt.Println("Got Request To / route")
 	// 응답 값 인코딩하여 응답.
 	json.NewEncoder(response).Encode(`Received Request`)
+}
+
+// 파일 업로드 요청 라우트
+func fileUpHandler(response http.ResponseWriter, request *http.Request) {
+	fmt.Println("Got Request to upload route")
+
+	// 요청 방식이 POST가 아닐 때 요청 거절 - 얼리 리턴
+	if request.Method != "POST" {
+		log.Fatal("Request Methos Is Not POST")
+
+		// 응답 인코딩
+		json.NewEncoder(response).Encode("Request Methos Is Not POST")
+
+		return
+	}
+
+	reader, err := request.MultipartReader()
+
+	if err != nil {
+		http.Error(response, err.Error(), http.StatusInternalServerError)
+
+		return
+	}
 }
