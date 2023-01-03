fbd63a06bcee836484eca0b0aa6e9a59767830a5
Kim Donghyun <kimdonghyun@Kimui-MacBookAir.local>
2023-01-03 16:14:42 +0900

    [CHANGED] integrated

--- tcpServerEx/database/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/donghquinn/databaseOpen
-
-go 1.19
-
-require (
-	github.com/go-sql-driver/mysql v1.7.0
-	github.com/joho/godotenv v1.4.0
-)
--- tcpServerEx/main.go
+++ tcpServerEx/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"github.com/joho/godotenv"
 )
+
+var countResult string
 
 func main() {
 	http.HandleFunc("/result", resultHandler)
@@ -16,8 +25,71 @@ func main() {
 }
 
 func resultHandler(response http.ResponseWriter, request *http.Request) {
+	mysql()
+
 	json.NewEncoder(response).Encode("Hi There")
 
-	fmt.Println("Responded")
+	responseBase := `total Counts: %s`
 
+	responsing := fmt.Sprintf(responseBase, countResult)
+
+	json.NewEncoder(response).Encode(responsing)
+
+}
+
+func mysql() {
+	getEnv()
+
+	db := openDb()
+	queryCount(db)
+}
+
+func getEnv() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func openDb() *sql.DB {
+	// 환경변수 로드
+	dbPasswd := os.Getenv("DB_PASS")
+	dbUser := os.Getenv("DB_USER")
+	dbHost := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+
+	// fmt.Println(dbUser, dbPasswd, dbName, dbHost, dbHost, dbPort)
+
+	// fmt.Println(dbUser + ":" + dbPasswd + "@" + "tcp" + "(" + dbHost + ":" + dbPort + ")" + "/" + dbName)
+
+	// Database 연결
+	database, err := sql.Open("mysql", dbUser+":"+dbPasswd+"@"+"tcp"+"("+dbHost+":"+dbPort+")"+"/"+dbName)
+
+	// Database 연결 설정
+	database.SetMaxOpenConns(10)
+	database.SetMaxIdleConns(10)
+
+	if err != nil {
+		log.Fatal(err)
+		panic(err.Error())
+	}
+
+	return database
+}
+
+func queryCount(database *sql.DB) {
+	count, err := database.Query("SELECT COUNT(name) as count FROM client")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for count.Next() {
+		count.Scan(&countResult)
+
+		fmt.Println(countResult)
+	}
 }
