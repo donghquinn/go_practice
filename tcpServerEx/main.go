4a032ecbb76abd048a06d3b2312af242dbd5eb87
Kim Donghyun <kimdonghyun@Kimui-MacBookAir.local>
2023-01-03 15:59:29 +0900

    [CHANGED]

--- tcpServerEx/main.go
+++ tcpServerEx/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/result", resultHandler)
 
+	fmt.Println("Server Start")
 	// server start
 	http.ListenAndServe(":8080", nil)
 
@@ -15,4 +17,7 @@ func main() {
 
 func resultHandler(response http.ResponseWriter, request *http.Request) {
 	json.NewEncoder(response).Encode("Hi There")
+
+	fmt.Println("Responded")
+
 }
